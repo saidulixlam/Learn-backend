142930c1f76f3b938841fd4153d085c228d56568
saidulixlam <saidulixlam@gmail.com>
2024-02-19 11:33:59 +0530

    2nd  backend commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log('Hello world');
+const multiply = (a,b)=>{
+    return a*b
+}
+console.log(multiply(2,4));
+
+const student={
+    name:"Saidul",
+    age:24,
+    getName(){
+        console.log('Hi my name is '+ this.name);
+    }
+}
+student.getName();
