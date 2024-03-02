9f76a6767e5593f25f072c7dd25d37e9225af24e
saidulixlam <saidulixlam@gmail.com>
2024-03-02 20:20:49 +0530

    express

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const http = require('http');
-const routes=require('./routes');
-const server = http.createServer(routes);
+const express = require('express');
 
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+const app = express();
+
+app.use((req,res,next)=>{
+  console.log('First middle ware');
+  next();
+})
+
+app.use((req,res,next)=>{
+  console.log('Second middle ware');
+  next();
+
+})
+
+app.use((req,res)=>{
+  res.send('Now printing')
+})
+
+app.listen(3000);
+
